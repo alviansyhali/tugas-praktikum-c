46b17b8b02e1d23e2e19dcd40c8e6bfe54adce67
Alviansyah Alimin <122435811+alviansyhali@users.noreply.github.com>
2023-10-12 14:00:34 +0700

    Add files via upload

---
+++ tugas-mandiri-1/tugas-mandiri-1.1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    char nim[] = "2143002";
+    char nama[] = "Alviansyah Alimin";
+    char jurusan[] = "Teknik Informatika";
+
+    printf("NIM    : %s\n", nim);
+    printf("NAMA   : %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+}
---
+++ tugas-mandiri-1/tugas-mandiri-1.2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int panjang_persegi = 10;
+    int lebar_persegi = 7;
+    int luas = panjang_persegi*lebar_persegi;
+
+    printf("luas = %d" , luas);
+    return 0;
+}
